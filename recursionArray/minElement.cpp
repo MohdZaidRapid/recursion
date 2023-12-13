f7d882f228589236495b1eda92bd2791c398ca56
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2023-12-14 04:38:06 +0530

    added

--- recursionArray/minElement.cpp
+++ recursionArray/minElement.cpp
@@ -2,7 +2,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int minElement(int arr[], int index, int n)
+{
+    if (index == n - 1)
+    {
+        return arr[index];
+    }
 
-int main() {
+    return min(arr[index], minElement(arr, index + 1, n));
+}
+
+int main()
+{
+    int arr[] = {3, 4, 5, 6, 7, 1, 0};
+    cout << minElement(arr, 0, 7);
     return 0;
 }
