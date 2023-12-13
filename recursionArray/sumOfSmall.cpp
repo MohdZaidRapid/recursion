48b3f4089d93c30f7c756c31dfc46ee7c8f117a5
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2023-12-14 04:21:48 +0530

    added

--- recursionArray/sumOfSmall.cpp
+++ recursionArray/sumOfSmall.cpp
@@ -2,9 +2,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int SumArray(int arr[], int index, int n)
+{
+    if (index == n)
+    {
+        return 0;
+    }
+    return arr[index] + SumArray(arr, index + 1, n);
+}
 
+int main()
+{
 
-
-int main() {
+    int arr[] = {3, 4, 5, 8, 2};
+    cout << SumArray(arr, 0, 5);
     return 0;
 }
