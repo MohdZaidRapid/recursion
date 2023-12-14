5934381e821604d0c706d16836d4d9a7ce66eeb4
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2023-12-14 15:19:58 +0530

    added

--- recursionString/reverseString.cpp
+++ recursionString/reverseString.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 #include <bits/stdc++.h>
+
 using namespace std;
+void reverseString(string &str, int start, int end)
+{
+    if (start >= end)
+    {
+        cout << str;
+        return;
+    }
+    char c = str[start];
+    str[start] = str[end];
+    str[end] = c;
+
+    reverseString(str, start + 1, end - 1);
+}
 int main()
 {
+    string str = "zaid";
+    reverseString(str, 0, 3);
     return 0;
 }
