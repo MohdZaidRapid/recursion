0cf48011707be19bf2ebbf7e1d05e01d166d8956
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2023-12-14 15:13:13 +0530

    added

--- recursionString/recur1.cpp
+++ recursionString/recur1.cpp
@@ -2,8 +2,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool checkPal(string str, int start, int end)
+{
+    if (start >= end)
+    {
+        return 1;
+    }
+    if (str[start] != str[end])
+    {
+        return 0;
+    }
+    else
+    {
+        return checkPal(str, start + 1, end - 1);
+    }
+}
 
 int main()
 {
+    string str = "namon";
+    cout << checkPal(str, 0, 4);
+
     return 0;
 }
