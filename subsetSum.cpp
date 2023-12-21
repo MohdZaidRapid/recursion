eaeb6c70cf25a977cbebfda192f392959b63e149
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2023-12-21 16:00:23 +0530

    added

--- subsetSum.cpp
+++ subsetSum.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 #include <bits/stdc++.h>
+using namespace std;
 
+void printArraySum(int arr[], int index, int n, int sum)
+{
+    if (n == index)
+    {
+        cout << sum << endl;
+        return;
+    }
+
+    printArraySum(arr, index + 1, n, sum);
+    printArraySum(arr, index + 1, n, sum + arr[index]);
+}
 
 using namespace std;
-int main() {
+int main()
+{
+    int arr[] = {1,2,3,4};
+    int sum = 0;
+    printArraySum(arr, 0, 4, sum);
+
     return 0;
 }
