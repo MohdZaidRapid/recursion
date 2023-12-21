d47300f1edd8212f706d453885174dcfd2404d15
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2023-12-21 17:20:50 +0530

    adde

--- SubsetSum/targetSum.cpp
+++ SubsetSum/targetSum.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+
+bool find(int arr[], int index, int n, int target)
+{
+    if (target == 0)
+    {
+        return 1;
+    }
+    if (index == n || target < 0)
+    {
+        return 0;
+    }
+
+    return find(arr, index + 1, n, target) || find(arr, index + 1, n, target - arr[index]);
+}
+
+int main()
+{
+    int arr[] = {3, 6, 4, 5};
+    int target = 12;
+
     return 0;
 }
