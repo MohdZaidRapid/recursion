385763bcba96eab4c31b6ddca02cfd564fc9deb1
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2023-12-21 16:13:11 +0530

    adde

--- subsetSum.cpp
+++ subsetSum.cpp
@@ -2,24 +2,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void printArraySum(int arr[], int index, int n, int sum)
+void printArraySum(int arr[], int index, int n, int sum, vector<int> &ans)
 {
     if (n == index)
     {
-        cout << sum << endl;
+        ans.push_back(sum);
         return;
     }
 
-    printArraySum(arr, index + 1, n, sum);
-    printArraySum(arr, index + 1, n, sum + arr[index]);
+    printArraySum(arr, index + 1, n, sum, ans);
+    printArraySum(arr, index + 1, n, sum + arr[index], ans);
 }
 
 using namespace std;
 int main()
 {
-    int arr[] = {1,2,3,4};
-    int sum = 0;
-    printArraySum(arr, 0, 4, sum);
+    int arr[] = {1, 2, 3, 4};
+    vector<int> ans;
+    printArraySum(arr, 0, 4, 0, ans);
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << endl;
+    }
 
     return 0;
 }
+
+// time O(2^n)
+// space O(n)
