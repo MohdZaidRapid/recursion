ce25ceebdb182b5572e6dcf0c147f4b4eb8545d3
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-02-10 01:16:34 +0530

    aded

--- permutation/permutwithoutextraspace.cpp
+++ permutation/permutwithoutextraspace.cpp
@@ -4,7 +4,17 @@ using namespace std;
 
 void permute(vector<int> &arr, vector<vector<int>> &ans, int index)
 {
-    
+    if (index == arr.size())
+    {
+        ans.push_back(arr);
+        return;
+    }
+    for (int i = index; i < arr.size(); i++)
+    {
+        swap(arr[i], arr[index]);
+        permute(arr, ans, index + 1);
+        swap(arr[i], arr[index]);
+    }
 }
 int main()
 {
